107dc21ccf4aa9690fd6cb9de91079af5f493dd0
Zbynek Zamazal <Zbynek.Zamazal.extern@boerse-stuttgart.de>
2023-11-10 12:46:54 +0100

    Request limiting middleware

--- REST.Core/Program.cs
+++ REST.Core/Program.cs
@@ -1,15 +1,32 @@
 using REST.Core.Middleware;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register limiting middleware (max 10 requests within 10-minutes time window)
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.User.Identity?.Name ?? httpContext.Request.Headers.Host.ToString(),
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 10,
+                QueueLimit = 0,
+                Window = TimeSpan.FromMinutes(10)
+            }));
+    options.RejectionStatusCode = 429;  // 429 Too Many Requests
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,5 +43,6 @@ app.MapControllers();
 
 // Register middleware (will be executed with each HTTP request)
 app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseRateLimiter();
 
 app.Run();
