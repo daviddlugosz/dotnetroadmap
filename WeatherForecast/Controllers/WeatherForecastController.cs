da3ffca1ea80d365ce49eadc8c5ebe9d3b4d5d63
Zbynek Zamazal <Zbynek.Zamazal.extern@boerse-stuttgart.de>
2024-01-02 20:41:20 +0100

    jwt middleware authentication registration, JwtSettings, testing endpoints

--- WeatherForecast/Controllers/WeatherForecastController.cs
+++ WeatherForecast/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WeatherForecast.Controllers
@@ -6,11 +7,6 @@ namespace WeatherForecast.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,16 +14,18 @@ namespace WeatherForecast.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        [Route("GetPublicData")]
+        public string GetPublicData()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "some public data";
+        }
+
+        [HttpGet]
+        [Route("GetRestrictedData")]
+        public string GetPrivateData()
+        {
+            return "PRIVATE DATA";
         }
     }
 }
--- WeatherForecast/Program.cs
+++ WeatherForecast/Program.cs
@@ -1,4 +1,40 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+var config = builder.Configuration;
+
+//var1
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
+//var2 - with explicit manual scheme selection and JwtBearer settings
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true // if SIGNATURE is valid then rest of properties must be valid as well because for the hash to be valid both header and payload have to also be valid in a first place (=> no need to check other properties)
+    };
+});
+
+builder.Services.AddAuthorization();
+
+
+
 
 // Add services to the container.
 
@@ -18,7 +54,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
